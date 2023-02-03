d4cbcc73e63cfd4db304cf28b20ed70dfdb70163
punarsidhu13 <123929875+punarsidhu13@users.noreply.github.com>
2023-02-03 12:30:04 +0530

    Add files via upload

---
+++ entr10.py
@@ -0,0 +1,45 @@
+# entering 10 entries with for loop
+
+ip_list = []
+for i in range(10):
+    a = input("enter data \n ")
+    ip_list.append(a)
+    print(ip_list)
+print(ip_list)
+
+
+# without for loop
+
+ip_list = []
+
+a = input("enter name 1 \n ")
+ip_list.append(a)
+
+a = input("enter name 2 \n ")
+ip_list.append(a)
+
+a = input("enter name 3 \n ")
+ip_list.append(a)
+
+a = input("enter name 4 \n ")
+ip_list.append(a)
+
+a = input("enter name 5 \n ")
+ip_list.append(a)
+
+a = input("enter name 6 \n ")
+ip_list.append(a)
+
+a = input("enter name 7 \n ")
+ip_list.append(a)
+
+a = input("enter name 8 \n ")
+ip_list.append(a)
+
+a = input("enter name 9 \n ")
+ip_list.append(a)
+
+a = input("enter name 10 \n ")
+ip_list.append(a)
+
+print(ip_list)
